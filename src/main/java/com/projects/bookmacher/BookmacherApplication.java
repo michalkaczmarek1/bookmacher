b20ce9bfca62bc23f2ac901d7040e3df0b3d862e
Michal Kaczmarek <mi123456ka@gmail.com>
2020-10-26 21:59:18 +0100

    First commit

---
+++ src/main/java/com/projects/bookmacher/BookmacherApplication.java
@@ -0,0 +1,13 @@
+package com.projects.bookmacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmacherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmacherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/bookmacher/BookmacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.bookmacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmacherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
